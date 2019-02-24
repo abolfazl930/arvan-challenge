8c4bb9526245185a4c7ec7046856c8d2a1a68d9e
abolfazl930 <a.abolfazl930@gmail.com>
2019-02-25 00:48:15 +0330

    created home page

---
+++ src/pages/home/hero-section.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Container from "react-strap-grid/container";
+import Row from "react-strap-grid/row";
+import Col from "react-strap-grid/col";
+
+import "./hero-section.css";
+
+class Hero extends Component {
+  render() {
+    return (
+      <section className="hero-section">
+        <Container>aaa</Container>
+      </section>
+    );
+  }
+}
+
+export default Hero;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Hero from "./hero-section";
+import "./styles.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <Hero />
+      </>
+    );
+  }
+}
+
+export default Home;
