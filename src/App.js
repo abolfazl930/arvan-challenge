31aec1687d42d547444cda9d0700cda8d2531ecb
abolfazl930 <a.abolfazl930@gmail.com>
2019-02-25 00:48:37 +0330

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Home from "./pages/home";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <>
+        <Home/>
       </>
     );
   }
