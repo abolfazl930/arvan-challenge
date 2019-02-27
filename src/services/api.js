89bf181f2c4cfe6ae0760fdd3b420393d4c5bdb0
abolfazl930 <a.abolfazl930@gmail.com>
2019-02-28 01:54:46 +0330

    added fetches

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,36 @@
-export const get = () => { };
+import urls from "../configs/urls";
+import { convertObjectToQueryString } from "../utils/convert-object";
 
-export const post = () => { };
+export const post = ({ api, model } = {}) => {
+  return fetch(`${urls.endPointAdmin}${api}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(model)
+  });
+};
 
-export const put = () => { };
+export const get = ({ api, model } = {}) => {
+  const query = convertObjectToQueryString(model);
+  return fetch(`${urls.endPointAdmin}${api}?${query}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  });
+};
+
+export const put = ({ api, model } = {}) => {
+  const query = convertObjectToQueryString(model);
+  return fetch(`${urls.endPointAdmin}${api}?${query}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(model)
+  });
+};
