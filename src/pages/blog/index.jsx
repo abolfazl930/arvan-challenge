091e3e0c6d3533e5cd671ed06245471e22c262f2
abolfazl930 <a.abolfazl930@gmail.com>
2019-02-28 01:51:55 +0330

    edited structure

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -2,14 +2,17 @@ import React, { Component } from "react";
 import Container from "react-strap-grid/container";
 import Row from "react-strap-grid/row";
 import Col from "react-strap-grid/col";
-
+import Articles from "./blog-articles";
+import "./styles.css";
 class Blog extends Component {
   render() {
     return (
-      <section className="about-section">
+      <section className="blog-section">
         <Container>
           <Row>
-            <Col md="8">a</Col>
+            <Col md="8">
+              <Articles />
+            </Col>
             <Col md="4">b</Col>
           </Row>
         </Container>
