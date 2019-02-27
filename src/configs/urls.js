0463507acc728f6a2a76e9c4cc62a988fc8dfbdf
abolfazl930 <a.abolfazl930@gmail.com>
2019-02-28 01:50:22 +0330

    added api url

--- src/configs/urls.js
+++ src/configs/urls.js
@@ -1,18 +1,18 @@
-import enums from '../enums';
+import enums from "../enums";
 
-const common = {
-
-};
+const common = {};
 
 const development = {
-  endPointAdmin: 'http://localhost:1111/api/v1',
+  endPointAdmin: "http://api.2lsbox.ir/api/pakziSite/"
 };
 
 const production = {
-  endPointAdmin: 'http://serverhost.domain/api/v1',
+  endPointAdmin: "http://serverhost.domain/api/v1"
 };
 
 export default {
   ...common,
-  ...(process.env.APP_ENV === enums.APP_ENV_PRODUCTION ? production : development),
+  ...(process.env.APP_ENV === enums.APP_ENV_PRODUCTION
+    ? production
+    : development)
 };
