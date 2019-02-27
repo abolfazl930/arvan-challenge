1f88297e42103d0285d509cb2975d77fa1f18dcc
abolfazl930 <a.abolfazl930@gmail.com>
2019-02-27 18:06:44 +0330

    added routes

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -1,1 +1,17 @@
-export default {};
+import { lazy } from "react";
+
+const routes = [
+  {
+    title: "home",
+    path: "/",
+    exact: true,
+    component: () => lazy(() => import("../pages/home"))
+  },
+  {
+    title: "blog",
+    path: "/blog",
+    exact: true,
+    component: () => lazy(() => import("../pages/blog"))
+  }
+];
+export default routes;
