42378f77a17368c7dc80deb4d893f9ade1bb9729
abolfazl930 <a.abolfazl930@gmail.com>
2019-02-27 18:06:27 +0330

    updated

---
+++ src/components/header/index.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Button from "../../microcomponents/button";
+import Container from "react-strap-grid/container";
+import "./styles.css";
+class Header extends Component {
+  render() {
+    const { routes } = this.props;
+    return (
+      <header class="header d-flex">
+        <Container>
+          <nav className="header__nav d-flex justify-content-between align-items-center">
+            <figure class="header__nav__logo">logo</figure>
+            <div className="d-flex align-items-center">
+              <ul className="header__nav__menu">
+                {routes.map((route, index) => (
+                  <li>
+                    <Link
+                      className="header__nav__menu__menu-item"
+                      exact={route.exact}
+                      key={index}
+                      to={route.path}
+                    >
+                      {route.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <Button
+                type="button"
+                color="suceess"
+                className="header__account-btn"
+              >
+                Create Account
+              </Button>
+            </div>
+          </nav>
+          <div className="header__title d-flex flex-column align-items-center justify-content-center">
+            <h1>Our Blog</h1>
+            <p>home - blog</p>
+          </div>
+        </Container>
+      </header>
+    );
+  }
+}
+
+export default Header;
