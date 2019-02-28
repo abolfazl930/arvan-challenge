90c0013686487dff4cd9c67f79d683e71e1c721e
abolfazl930 <a.abolfazl930@gmail.com>
2019-03-01 02:25:15 +0330

    added date in card

--- src/microcomponents/card/index.jsx
+++ src/microcomponents/card/index.jsx
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 import "./styles.css";
 class Card extends Component {
   render() {
-    const { img, title, content } = this.props;
+    const { img, title, content, date } = this.props;
     return (
       <div className="card">
         <figure className="card__header">
           <img src={img} />
         </figure>
-        <div class="card__body">
+        <div className="card__body">
           <h4 className="card__body__title">{title}</h4>
+          <p className="card__body__date">{date}</p>
           <p className="card__body__content">{content}</p>
         </div>
       </div>
