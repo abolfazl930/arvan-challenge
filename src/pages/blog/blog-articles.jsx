cd58bb5456ac252c0989754a62f1ab3be1ac3aea
abolfazl930 <a.abolfazl930@gmail.com>
2019-03-01 02:26:45 +0330

    dynamic blog page

--- src/pages/blog/blog-articles.jsx
+++ src/pages/blog/blog-articles.jsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import Row from "react-strap-grid/row";
 import Col from "react-strap-grid/col";
 import Card from "../../microcomponents/card";
+import Loader from "../../microcomponents/loader";
 import admin from "../../services/admin";
+import { trimString } from "../../utils/trime-string";
+import { formatDate } from "../../utils/convert-date-format";
+import { Link } from "react-router-dom";
 
 import "./blog-articles.css";
 
@@ -10,7 +14,7 @@ class Articles extends Component {
   state = {
     articles: [],
     page: 1,
-    pageSize: 10
+    pageSize: 6
   };
   componentDidMount() {
     this.loadArticles();
@@ -29,21 +33,24 @@ class Articles extends Component {
     );
   };
   render() {
+    const { articles } = this.state;
     return (
       <div className="articles-wrapper">
         <Row>
-          <Col md="6">
-            <Card title="title" content="description" />
-          </Col>
-          <Col md="6">
-            <Card title="title" content="description" />
-          </Col>
-          <Col md="6">
-            <Card title="title" content="description" />
-          </Col>
-          <Col md="6">
-            <Card title="title" content="description" />
-          </Col>
+          {articles.length === 0 ? (
+            <Loader />
+          ) : (
+            articles.map((article, index) => (
+              <Col md="6" key={index}>
+                <Card
+                  img={article.image}
+                  title={<Link to="test">{article.title}</Link>}
+                  date={formatDate(Number(article.createdAt))}
+                  content={trimString(article.summary, 80)}
+                />
+              </Col>
+            ))
+          )}
         </Row>
       </div>
     );
