a6deb3014aec15a7ea2bb3676efe3c67ef15a6bd
abolfazl930 <a.abolfazl930@gmail.com>
2019-03-01 21:18:25 +0330

    edited structure

--- src/pages/blog/blog-articles.jsx
+++ src/pages/blog/blog-articles.jsx
@@ -3,7 +3,6 @@ import Row from "react-strap-grid/row";
 import Col from "react-strap-grid/col";
 import Card from "../../microcomponents/card";
 import Loader from "../../microcomponents/loader";
-import admin from "../../services/admin";
 import { trimString } from "../../utils/trime-string";
 import { formatDate } from "../../utils/convert-date-format";
 import { Link } from "react-router-dom";
@@ -11,29 +10,8 @@ import { Link } from "react-router-dom";
 import "./blog-articles.css";
 
 class Articles extends Component {
-  state = {
-    articles: [],
-    page: 1,
-    pageSize: 6
-  };
-  componentDidMount() {
-    this.loadArticles();
-  }
-  loadArticles = async () => {
-    const { page, pageSize } = this.state;
-    let response = null;
-    response = await admin.article.list({ page, pageSize });
-    response = await response.json();
-    const articles = response.data.value.result;
-    this.setState(
-      {
-        articles
-      },
-      () => console.log("res:", this.state.articles)
-    );
-  };
   render() {
-    const { articles } = this.state;
+    const { articles } = this.props;
     return (
       <div className="articles-wrapper">
         <Row>
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -2,18 +2,52 @@ import React, { Component } from "react";
 import Container from "react-strap-grid/container";
 import Row from "react-strap-grid/row";
 import Col from "react-strap-grid/col";
+import admin from "../../services/admin";
 import Articles from "./blog-articles";
+import SearchBox from "./blog-search-box";
+import Category from "./blog-Category";
+import LatestArticles from "./latest-articles";
 import "./styles.css";
 class Blog extends Component {
+  state = {
+    articles: [],
+    page: 1,
+    pageSize: 6
+  };
+  componentDidMount() {
+    this.loadArticles();
+  }
+  loadArticles = async () => {
+    const { page, pageSize } = this.state;
+    let response = null;
+    response = await admin.article.list({ page, pageSize });
+    response = await response.json();
+    const articles = response.data.value.result;
+    this.setState(
+      {
+        articles
+      },
+      () => console.log("Articles:", this.state.articles)
+    );
+  };
   render() {
+    const { articles } = this.state;
     return (
       <section className="blog-section">
         <Container>
           <Row>
             <Col md="7">
-              <Articles />
+              <Articles articles={articles} />
             </Col>
-            <Col md="5">b</Col>
+            <Col md="3">
+              <Row>
+                <Col>
+                  <SearchBox />
+                  <Category />
+                  <LatestArticles articles={articles} />
+                </Col>
+              </Row>
+            </Col>
           </Row>
         </Container>
       </section>
