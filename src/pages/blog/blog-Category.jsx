9ef4db8f9617f22cb7f37f2d9a8e3bc0e5aba804
abolfazl930 <a.abolfazl930@gmail.com>
2019-03-01 21:10:23 +0330

    created blog-category component

---
+++ src/pages/blog/blog-Category.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./blog-category.css";
+
+class SearchBox extends React.Component {
+  state = {
+    categories: []
+  };
+
+  componentDidMount() {
+    this.loadCategory();
+  }
+
+  loadCategory = () => {
+    this.setState({
+      categories: [
+        "Duplex Home",
+        "Drawing Rooms",
+        "Bedrooms",
+        "Kitchen Rooms",
+        "Bathrooms",
+        "Garden Home",
+        "Animation Design"
+      ]
+    });
+  };
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div className="category-container">
+        <h4 className="category-container__title">Categories</h4>
+        <ul className="category-container__list">
+          {categories.map(category => (
+            <li className="category-container__list__item">
+              <Link to="#">{category}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default SearchBox;
