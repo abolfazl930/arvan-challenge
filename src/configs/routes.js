f43a836047e7e7e62ec188d8cebdc467c7d2a588
abolfazl930 <a.abolfazl930@gmail.com>
2019-03-03 00:44:46 +0330

    added visibleRoutes modual

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -5,13 +5,31 @@ const routes = [
     title: "home",
     path: "/",
     exact: true,
+    visibleIn: ["header", "footer"],
     component: () => lazy(() => import("../pages/home"))
   },
   {
     title: "blog",
     path: "/blog",
     exact: true,
+    visibleIn: ["header", "footer"],
     component: () => lazy(() => import("../pages/blog"))
+  },
+  {
+    title: "",
+    path: "/blog/:slug",
+    exact: true,
+    visibleIn: [],
+    component: () => lazy(() => import("../pages/single-article"))
   }
 ];
 export default routes;
+
+export const visibleRoutes = ({ visibleIn } = {}) =>
+  routes.filter(route => {
+    if (visibleIn) {
+      return route.visibleIn.find(visible => visible === visibleIn[0]);
+    } else {
+      return route.visibleIn.length > 0;
+    }
+  });
