05bef105faeac53e09a08d385b01ad04d7fac9e9
abolfazl930 <a.abolfazl930@gmail.com>
2019-03-04 02:12:22 +0330

    created header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,47 +1,14 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import routes, { visibleRoutes } from "../../configs/routes";
-import Button from "../../microcomponents/button";
-import Container from "react-strap-grid/container";
 import "./styles.css";
 class Header extends Component {
   render() {
-    // const { routes } = this.props;
+    const { title, content } = this.props;
     return (
-      <header className="header d-flex">
-        <Container>
-          <nav className="header__nav d-flex justify-content-between align-items-center">
-            <figure className="header__nav__logo">logo</figure>
-            <div className="d-flex align-items-center">
-              <ul className="header__nav__menu">
-                {visibleRoutes({ visibleIn: ["header"] }).map(
-                  (route, index) => (
-                    <li key={index}>
-                      <Link
-                        className="header__nav__menu__menu-item"
-                        exact={route.exact}
-                        to={route.path}
-                      >
-                        {route.title}
-                      </Link>
-                    </li>
-                  )
-                )}
-              </ul>
-              <Button
-                type="button"
-                color="suceess"
-                className="header__account-btn"
-              >
-                Create Account
-              </Button>
-            </div>
-          </nav>
-          <div className="header__title d-flex flex-column align-items-center justify-content-center">
-            <h1>Our Blog</h1>
-            <p>home - blog</p>
-          </div>
-        </Container>
+      <header className="header d-flex justify-content-center align-items-center">
+        <div className="header__title d-flex flex-column align-items-center justify-content-center">
+          <h1>{title}</h1>
+          <p>{content}</p>
+        </div>
       </header>
     );
   }
