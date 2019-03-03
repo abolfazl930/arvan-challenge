ac5a426af4e4ce38fa89022ed06d55858f71500f
abolfazl930 <a.abolfazl930@gmail.com>
2019-03-04 02:09:53 +0330

    added pagination

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -5,12 +5,17 @@ import Row from "react-strap-grid/row";
 import Col from "react-strap-grid/col";
 
 import admin from "../../services/admin";
-
-import Articles from "../../components/blog-articles";
+import Header from "../../components/header";
 import SearchBox from "../../components/blog-search-box";
 import Category from "../../components/blog-categry";
 import LatestArticles from "../../components/latest-articles";
 import News from "../../components/news";
+import Card from "../../microcomponents/card";
+import Loader from "../../microcomponents/loader";
+import { trimString } from "../../utils/trime-string";
+import { formatDate } from "../../utils/convert-date-format";
+import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 
 import "./styles.css";
 class Blog extends Component {
@@ -35,30 +40,83 @@ class Blog extends Component {
       () => console.log("Articles:", this.state.articles)
     );
   };
+  handlePageClick = data => {
+    let selected = data.selected + 1;
+    console.log("selected", selected);
+    this.setState(
+      {
+        page: selected
+      },
+      () => {
+        console.log("state:", this.state.page);
+      }
+    );
+  };
   render() {
     const { articles } = this.state;
     return (
       <section className="blog-section">
-        <Container>
-          <Row>
-            <Col md="7">
-              <Articles articles={articles} />
-            </Col>
-            <Col md="3">
-              <Row>
+        <Header title="Our Blog" content="home - blog" />
+        <main>
+          <Container>
+            <Row>
+              <Col md="7">
+                <div className="articles-wrapper">
+                  <Row>
+                    {articles.length === 0 ? (
+                      <Loader />
+                    ) : (
+                      articles.map((article, index) => (
+                        <Col md="6" key={index}>
+                          <Card
+                            img={article.image}
+                            title={
+                              <Link to={`/blog/${article.slug}`}>
+                                {article.title}
+                              </Link>
+                            }
+                            date={formatDate(Number(article.createdAt))}
+                            content={trimString(article.summary, 80)}
+                          />
+                        </Col>
+                      ))
+                    )}
+                  </Row>
+                </div>
                 <Col>
-                  <SearchBox />
-                  <Category />
-                  <LatestArticles articles={articles} />
+                  <Row className="d-flex justify-content-center">
+                    <ReactPaginate
+                      previousLabel={"< previous"}
+                      nextLabel={"next >"}
+                      breakLabel={"..."}
+                      breakClassName={"break-me"}
+                      pageCount={30}
+                      marginPagesDisplayed={1}
+                      pageRangeDisplayed={3}
+                      onPageChange={this.handlePageClick}
+                      containerClassName={"pagination d-flex"}
+                      subContainerClassName={"pages pagination"}
+                      activeClassName={"active"}
+                    />
+                  </Row>
                 </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-        <News
-          title="Subscribe Our Newsletter"
-          content="Donec tempor finibus ante ac luctus. Fusce facilisis nisi vel odio tincidunt maximus. Pellentesque tempus gravida viverra."
-        />
+              </Col>
+              <Col md="3">
+                <Row>
+                  <Col>
+                    <SearchBox />
+                    <Category />
+                    <LatestArticles articles={articles} />
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+          <News
+            title="Subscribe Our Newsletter"
+            content="Donec tempor finibus ante ac luctus. Fusce facilisis nisi vel odio tincidunt maximus. Pellentesque tempus gravida viverra."
+          />
+        </main>
       </section>
     );
   }
