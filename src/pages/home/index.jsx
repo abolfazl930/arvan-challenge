218e40aa490039da27274762c75aaffaaa50f499
abolfazl930 <a.abolfazl930@gmail.com>
2019-03-04 02:08:46 +0330

    updated home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-import Hero from "./hero-section";
+import imgHomeBg from "../../assets/images/home-background-bg.svg";
 import "./styles.css";
 
 class Home extends Component {
   render() {
     return (
-      <>
-        <div>home</div>
-      </>
+      <section className="hero-section">
+        <header className="hero-section__header">
+          <figure className="header-bg">
+            <img src={imgHomeBg} alt="home" />
+          </figure>
+        </header>
+      </section>
     );
   }
 }
