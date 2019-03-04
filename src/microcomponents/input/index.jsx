f34a1c4a5ac2194cbb1dfc25d61bb8944f32a34d
abolfazl930 <a.abolfazl930@gmail.com>
2019-03-04 20:08:20 +0330

    edited input microcomponent

--- src/microcomponents/input/index.jsx
+++ src/microcomponents/input/index.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import "./styles.css";
 
 class Input extends React.Component {
+  onChange = event => {
+    const { onChange } = this.props;
+    onChange(event.target.value);
+  };
   render() {
-    const { placeholder, type, className } = this.props;
+    const { placeholder, type, className, onChange, value } = this.props;
     return (
       <input
         className={["input-element", className].join(" ")}
         placeholder={placeholder}
+        onChange={onChange}
         type={type}
+        value={value}
       />
     );
   }
